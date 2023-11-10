abf05d16043861f4bb94393cbc9417bff083769d
Shivam Pandey <144382042+shivam1607github@users.noreply.github.com>
2023-11-10 21:46:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function convertCtoF() {
+    var celsius = document.getElementById("celsius").value;
+    var fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("fahrenheit").value = fahrenheit;
+}
+
+function convertFtoC() {
+    var fahrenheit = document.getElementById("fahrenheit").value;
+    var celsius = (fahrenheit - 32) * 5/9;
+    document.getElementById("celsius").value = celsius;
+}
